b2088848f5911aa70cab3aceec12f3f713bb8c66
juru-juliette <kangabejuliette@gmail.com>
2019-09-13 15:57:42 +0200

    I created a article list function

--- app/requests.py
+++ app/requests.py
@@ -61,3 +61,44 @@ def get_article(id)
     '''
     Function that gets the json response to url request
     '''
+    get_article_url= article_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_article_url) as url:
+        get_article_data=url.read()
+        get_article_response=json.loads(get_article_data)
+
+        article_results= None
+        if get_article_response ['articles']:
+            article_results_list= get_source_response['articles']
+            article_results = process_results(article_results_list)
+
+    return source_results
+def receive_result(article_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+
+    Args:
+    article_list: A list of dictionaries that contain article details
+
+    Returns :
+    article_results: A list of article objects
+    '''
+
+    article_results=[]
+    for article in article_list:
+        id=article.get('id')
+        author=article.get('author')
+        title=article.get('title')
+        description=article.get('description')
+        url=article.get('url')
+        urlToImage=article.get('urlToImage')
+        publishedAt=article.get('publishedAt')
+        content=article.get('content')
+        
+        if author:
+            article_object = Article(id,author,title,description,url,urlToImage,publishedAt,content)
+            article_results.append(article_object)  
+
+    return article_results 
+
+
