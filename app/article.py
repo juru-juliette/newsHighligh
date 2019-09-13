90b6d3ca7ee7cd6ae5b4458a992485d479a80877
juru-juliette <kangabejuliette@gmail.com>
2019-09-13 15:38:25 +0200

    I created articles properties

---
+++ app/article.py
@@ -0,0 +1,14 @@
+class article:
+    '''
+    News class to define News Objects
+    '''
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
+        self.id=id
+        self.author=author
+        self.title=title
+        self.description=description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
+        self.content=content
+        
