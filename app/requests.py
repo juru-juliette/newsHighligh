05010c3ece2956d5f0c07c9b339e11900adf7dc5
juru-juliette <kangabejuliette@gmail.com>
2019-09-13 10:15:12 +0200

    creating function that processes the source result

--- app/requests.py
+++ app/requests.py
@@ -18,3 +18,48 @@ Source=source.Source
 api_key=app.config['SOURCE_API_KEY']
 # Getting the news base url
 base_url=app.config['SOURCE_API_BASE_URL']
+    def get_source(category):
+    '''
+    Function that gets the json response to url request
+    '''
+    get_source_url=base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data=url.read()
+        get_source_response=json.loads(get_source_data)
+
+        source_results= None
+
+
+        # if get_source_response ['sources']:
+        #     source_results_list=get_source_response['sources']
+
+        if get_source_response ['sources']:
+            source_results_list=get_source_response['sources']
+            source_results= process_results(source_results_list)
+
+    return source_results
+
+    def process_results(source_list):
+    '''            
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results=[]
+    for source in source_list:
+        id=source.get('id')
+        name=source.get('name')
+        description=source.get('description')
+
+        if  id:
+            source_Object = Source(id,name,description)
+            source_results.append(source_Object)  
+
+    return source_results    
+
+    
